f946bf93ecda26976c5cfc1bc437b8cbe7ab2054
devdajanvi2005-tech <devdajanvi2005@gmail.com>
2026-08-21 14:04:24 +0530

    main.py

---
+++ p6/main.py
@@ -0,0 +1,18 @@
+from students import get_students
+from ranking import rank_students
+from report import display
+
+try:
+    n = int(input("Enter number of students: "))
+
+    students = []
+
+    for i in range(n):
+        print("\nStudent", i + 1)
+        students.append(get_students())
+
+    students = rank_students(students)
+    display(students)
+
+except ValueError:
+    print("Invalid input")
